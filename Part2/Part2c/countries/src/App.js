217d6a920ce08784b7178b983969dd486d0183f6
Sarju Mulmi <mulmi.sarju@gmail.com>
2019-09-03 11:46:25 -0400

    part2c countries step 2.14 exercise answers added

--- Part2/Part2c/countries/src/App.js
+++ Part2/Part2c/countries/src/App.js
@@ -4,11 +4,15 @@ import axios from 'axios'
 function App() {
   const [countries, setCountries] = useState([])
   const [searchCountry, setSearchCountry] = useState('')
+  const [showCountry, setShowCountry] = useState(false)
+  const [countryDetail, setCountryDetail] = useState({})
+  const [weatherDetail, setWeatherDetail] = useState({})
 
   const handleSearchCountry = evt => {
     const searchValue = evt.target.value
     setSearchCountry(searchValue)
-    
+    setShowCountry(false)
+    setCountryDetail({})
     axios.get('https://restcountries.eu/rest/v2/all')
       .then(resp => resp.data)
       .then(countries => {
@@ -17,13 +21,31 @@ function App() {
       })
   }
 
+  useEffect(() => {
+    if (countries.length === 1) {
+      const getWeatherDetail = async (location) => {
+        const resp =   await axios.get(`http://api.apixu.com/v1/current.json?key=fea88c71ab3f49d9b42135103190309&q=${location}`)
+        setWeatherDetail(resp.data.current)
+      }
+      getWeatherDetail(countries[0].capital)
+    }
+  }, [countries.map(c => c.name).join(',')])
+
+  const showCountryDetail = (country) => {
+    setShowCountry(true)
+    setCountryDetail(country)
+  }
+
   return (
     <div>
       find countries: <input value={searchCountry} onChange={handleSearchCountry} />
       {countries.length > 10 && <p>Too many countries, specify another filter</p>}
       {countries.length <= 10 && countries.length > 1 && 
         countries.map(country => (
-          <div key={country.name}>{country.name}</div>
+          <div key={country.name}>
+            {country.name}
+            <button onClick={() => showCountryDetail(country)}>show</button>
+          </div>
         ))
       }
       {countries.length === 1 && 
@@ -36,8 +58,23 @@ function App() {
             <div key={lang.name}>{lang.name}</div>
           ))}
           <picture><img src={countries[0].flag} style={{width: '150px',}} /></picture>
+          <h2>Weather in {countries[0].capital}</h2>
+          <p><strong>temperature: {weatherDetail.temp_c} Celcius</strong> </p>
+          {weatherDetail.condition && <picture><img src={'https:'+ weatherDetail.condition.icon} style={{width: '100px',}}/></picture>}
+          <p><strong>wind: {weatherDetail.wind_kph} direction {weatherDetail.wind_dir}</strong> </p>
         </div>
       }
+      {showCountry && 
+        <div>
+          <h2>{countryDetail.name}</h2>
+          <p>Capital {countryDetail.capital}</p>
+          <p>Population {countryDetail.population}</p>
+          <h3>Languages</h3>
+          {countryDetail.languages.map(lang => (
+            <div key={lang.name}>{lang.name}</div>
+          ))}
+          <picture><img src={countryDetail.flag} style={{width: '150px',}} /></picture>
+        </div>}
     </div>
   );
 }
